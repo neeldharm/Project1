d623591cabc1f68a110545eb272a60734e18d710
dharm <dharmneel21@gmail.com>
2025-10-23 01:51:42 -0700

    created and tested addItem and setup functions. Items only updated after reload right now

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import './App.css'
 import Nav from './Nav'
 import Item from './Item'
-// import { useState, useEffect } from 'react'
+// import { addItem } from './Item'
+import { useState, useEffect } from 'react'
 
 
 export default function App() {
+  const [items, setItems] = useState(setup());
+  const itemObjects : any[] = [];
+  items.forEach(key => {
+    itemObjects.push(
+      <Item item={key} type={items.get(key)} />
+    )
+  });
+
   return (
     <>
       <Nav />
       <div className="body">
+        <div>{itemObjects}</div>
         <Item item="wine1" type="bruh"/>
       </div>
     </>
   )
 }
 
-// function setup() {
-//   const [items, setItems] = useState([]);
+function setup(): Map<String, any> {
+  const items = new Map<String, any>();
+  Object.keys(localStorage).forEach(key => {
+  if (localStorage.getItem(key)) {
+    items.set(key, localStorage.getItem(key));
+  };
+  });
 
-// }
+  return items;
+}
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,22 +1,23 @@
 import './Item.css'
 
-export default function Item({item, type}: {item: string, type: string}) {
+export default function Item({item, type}: {item: string, type: string | null}) {
   return (
     <>
       <div className="card">
         <img alt="pfp" />
         <span className='space'>{item}</span>
-        <span className='space'>{type}</span>
-        <button onClick={() => {
-          addItem('item', new Map().set('itemcode', ['value1', 'value2']))}}>hello</button>
+        <span className='space'>{type ? type : "bruh"}</span>
+        <button onClick={() => {addItem('item1', 'itemcodes')}}></button>
       </div>
     </>
   )
 }
 
-function addItem(key: string, value: string | Map<string, Array<any>>) {
-  console.log("oh boy");
-  console.log(JSON.stringify(value));
-  localStorage.setItem(key, JSON.stringify(value));
+function addItem(key: string, value: string): void;
+function addItem(key: string, value: string, array: Array<any>): void;
+
+function addItem(key: string, value: string, array?: Array<any>) {
+  if (array) localStorage.setItem(key, value + ":" + array);
+  else localStorage.setItem(key, value);
 }
 // try overloading function instead
