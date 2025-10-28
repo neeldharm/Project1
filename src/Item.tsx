8e7719a75c8c31ce74a8c959e5dbb25bcd93587b
dharm <dharmneel21@gmail.com>
2025-10-27 17:06:26 -0700

    Added sidenav and continuing testing addItem, creating codes for each item

--- src/Item.tsx
+++ src/Item.tsx
@@ -7,17 +7,17 @@ export default function Item({item, type}: {item: string, type: string | null}) 
         <img alt="pfp" />
         <span className='space'>{item}</span>
         <span className='space'>{type ? type : "bruh"}</span>
-        <button onClick={() => {addItem('item1', 'itemcodes')}}></button>
+        {/* <button onClick={() => {addItem('item1', 'itemcodes')}}></button> */}
       </div>
     </>
   )
 }
 
-function addItem(key: string, value: string): void;
-function addItem(key: string, value: string, array: Array<any>): void;
+// function addItem(key: string, value: string): void;
+// function addItem(key: string, value: string, array: Array<any>): void;
 
-function addItem(key: string, value: string, array?: Array<any>) {
-  if (array) localStorage.setItem(key, value + ":" + array);
-  else localStorage.setItem(key, value);
-}
+// function addItem(key: string, value: string, array?: Array<any>) {
+//   if (array) localStorage.setItem(key, value + ":" + array);
+//   else localStorage.setItem(key, value);
+// }
 // try overloading function instead
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -7,9 +7,24 @@ export default function Nav() {
             <div className="headnav">
                 <button className="menu"><img alt="bruh" src={icon}/></button>
             </div>
-            <div>
-                
+            <div className="sidenav">
+                <button onClick={() => {
+                    addItem('home', 'itemcodes', ['code1', 'code2'])
+                }}>Add Item</button>
             </div>
         </>
     )
+}
+
+function addItem(key: string, value: string, array: Array<any>): void;
+function addItem(key: string, value: string): void;
+
+function addItem(key: string, value: string, array?: Array<any>) {
+    let code: string = '' + Math.round(Math.random() * 100000);
+    for (let i = 0; i < 8 - code.length; i++) {
+        code = '0' + code;
+    }
+    code = key + code;
+    if (array) localStorage.setItem(code, value + ":" + array);
+    else localStorage.setItem(code, value);
 }
